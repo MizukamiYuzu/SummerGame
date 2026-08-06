383d65c9e95106af9cc0517e8ecfd2841f1ba42e
MizukamiYuzu <fko2647069@stu.o-hara.ac.jp>
2026-08-07 02:48:53 +0900

    a

---
+++ randmint_kari/minto.cpp
@@ -0,0 +1,15 @@
+﻿#include "minto.h"
+
+minto::minto()
+{
+}
+
+minto::~minto()
+{
+}
+
+void minto::GetWither()
+{
+	m_plant = false;
+	m_energy = 0;
+}
---
+++ randmint_kari/minto.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+class minto
+{
+public:
+	minto();
+	~minto();
+
+	void SetPlant(int plant) { m_plant = plant; }
+	void GetWither();
+
+private:
+	int m_energy = 0;	// エネルギー
+	
+	bool m_plant;
+
+	
+};
+
